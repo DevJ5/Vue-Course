b7b0d4400defe06d7cba579e02e2e0c836bdc465
DevJ5 <idevelopjs@gmail.com>
2022-05-01 15:56:47 +0200

    Project 1

---
+++ project-1/src/components/learning-resources/AddResource.vue
@@ -0,0 +1,69 @@
+<template>
+  <base-card>
+    <form action="">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input type="text" id="title" name="title" ref="titleInput" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea
+          name="description"
+          id="description"
+          cols="30"
+          rows="3"
+          v-model="description"
+        ></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input type="url" id="link" name="link" ref="linkInput" />
+      </div>
+      <base-button type="submit">Add Resource</base-button>
+    </form>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      description: 'Enter a description',
+    };
+  },
+  methods: {
+    submitData() {
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredLink = this.$refs.linkInput.value;
+    },
+  },
+};
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
---
+++ project-1/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,66 @@
+<template>
+  <base-card>
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResourcesButtonMode"
+    >
+      Stored Resources
+    </base-button>
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResourceButtonMode"
+    >
+      Add Resource
+    </base-button>
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import AddResource from './AddResource.vue';
+import StoredResources from './StoredResources.vue';
+
+export default {
+  components: {
+    AddResource,
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+      selectedTab: 'stored-resources',
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+  provide() {
+    return {
+      storedResources: this.storedResources,
+    };
+  },
+  computed: {
+    storedResourcesButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResourceButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+};
+</script>
