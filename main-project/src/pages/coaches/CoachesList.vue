34edfc8c45feb495159a2df34d4c926f1e5d844e
DevJ5 <idevelopjs@gmail.com>
2022-06-11 15:04:33 +0200

    Start main project

---
+++ main-project/src/pages/coaches/CoachesList.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as a Coach</router-link>
+    </div>
+    <ul>
+      LIST OF COACHES
+      <router-link to="/coaches/bert"><li>Bert</li></router-link>
+    </ul>
+  </section>
+</template>
